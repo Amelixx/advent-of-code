334bee725106fd78b353c685827a912e11d9ed77
Amelix <rubix9006@gmail.com>
2021-12-02 17:16:21 +0000

    and so she made that commit, in pursuit of 1% more efficiency
    made the for loop have one less iteration

--- puzzles/1/index.ts
+++ puzzles/1/index.ts
@@ -5,7 +5,7 @@ export default () => {
     const numbers = input.split("\n")
 
     let count = 0
-    for (let i=0; i < numbers.length; i++) {
+    for (let i=0; i < numbers.length-1; i++) {
         if (parseInt(numbers[i]) < parseInt(numbers[i+1])) count ++
     }
     return count
