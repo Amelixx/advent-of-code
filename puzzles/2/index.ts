c037b7645a547f617439def9a204552ad55ddc33
Amelix <rubix9006@gmail.com>
2021-12-05 17:10:49 +0000

    Day 2 Part 2

--- puzzles/2/index.ts
+++ puzzles/2/index.ts
@@ -1,27 +1,42 @@
 import fs from 'fs'
 
-let horizontalPos = 0
-let depth = 0
+let horizontalPos = 0,
+    depth = 0,
+    aim = 0,
+    part2 = false
 
 export default () => {
     const input = fs.readFileSync("./puzzles/2/input", 'utf-8')
     const commands = input.split("\r")
 
+    move(commands)
+    let part1Result = horizontalPos * depth
+    part2 = true
+    horizontalPos = 0
+    depth = 0
+    move(commands)
+    
+    return [part1Result, horizontalPos * depth]
+}
+
+function move(commands: string[]) {
     for (let command of commands) {
         const [order, amount] = command.split(" ")
         eval(order + "(" + amount + ")")
     }
-    return [horizontalPos * depth]
 }
 
 function forward (amount: number) {
     horizontalPos += amount
+    if (part2) depth += aim * amount
 }
 
 function up (amount: number) {
-    depth -= amount
+    if (part2) aim -= amount
+    else depth -= amount
 }
 
 function down (amount: number) {
-    depth += amount
+    if (part2) aim += amount
+    else depth += amount
 }
